2e8dc6d30ff0f3b3040e3e9a70864f2174456245
Lavosierdq <lavosierbarbosa@gmail.com>
2023-04-28 23:37:25 -0300

    feat: extendendo outras configurações.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -20,14 +20,17 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
+-- navegar entre janelas utilizando Controle + h,j,k,l
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+keymap("n", "<leader>e", ":Lex 10<cr>", opts)
 
 -- Resize with arrows
+-- Redefinir as janelas com Control + Arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
 keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
